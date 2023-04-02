10e109a2f608604eae8560a90a5a0afaebba2a5d
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-04-02 21:47:22 +0700

    Update blog

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '60ce84f7deb54896804916a9854f010c',
+  rootNotionPageId: '93dbc670bb934b789b6f422b4d349c9a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
